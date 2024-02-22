a684666b2bdacbbef2011af1b8d47d36b986d8e4
xsenemy <160485189+xsenemy@users.noreply.github.com>
2024-02-22 20:33:23 +0100

    Update day4.py
    fix wrong instructions

--- day4.py
+++ day4.py
@@ -1,10 +1,17 @@
+#44 Programming questions Code
+#Ultra easy questions:
+#4) Sum all the integers from 0 -> 1000. Sum all the integers from [x] -> [y], assuming that "x" is smaller than "y".
+
 total = 0
 total_r = 0
 
+x = int(input("X value (smaller): "))
+y = int(input("Y value (larger): "))
+
 for i in range(1001):
     total += i
-for i in range(100, 0, -1):
+for i in range(x, y+1):
     total_r += i
 
 print(f"sum from 0 to 1000: {total}")
-print(f"sum from 100 to 0: {total_r}")
+print(f"sum from {x} to {y}: {total_r}")
