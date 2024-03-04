d059a163f5d9f43908732a7ba684676b02d7aeb4
xsenemy <160485189+xsenemy@users.noreply.github.com>
2024-03-04 18:34:14 +0100

    Update day13.py

--- day13.py
+++ day13.py
@@ -4,25 +4,42 @@
 
 import random
 
+
 values = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"]
-seeds = ["♣", "♦", "♥", "♠"]
+seeds = ["\u2660", "\u2661", "\u2663", "\u2662"]
 deck = []
+deck_shuffled = []
+
+print('Deck:')
 for i in seeds:
     new_list = []
     for j in values:
         new_list.append(str(j) + i)
     deck.append(new_list)
 
-print('Deck:')
 for i in range(len(deck)):
-    print(*deck[i])
-random.shuffle(deck)
-print('Shuffled Deck:')
-for i in range(len(deck)):
-    print(*deck[i])
-seed = random.randint(0, 3)
-card1 = random.choice(deck[seed])
-card2 = random.choice(deck[seed])
+    print(*deck[i], sep="\t")
+
+print('Deck Shuffled')
+char = []
+for i in range(4):
+    for j in range(len(deck[i])):
+        char.append(deck[i][j])
+random.shuffle(char)
+
+n=0
+for i in range(4):
+    new_list = []
+    for j in range(len(deck[i])):
+        new_list.append(char[n])
+        n += 1
+    deck_shuffled.append(new_list)
+
+for i in range(len(deck_shuffled)):
+    print(*deck_shuffled[i], sep="\t")
+
+card1 = random.choice(char)
+card2 = random.choice(char)
 while card2 == card1:
-    card2 = random.choice(deck[seed])
+    card2 = random.choice(char)
 print(f"\nCards: \t{card1}\t{card2}")
