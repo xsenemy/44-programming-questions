662740d3726c11ff777ef51db515af03f25be271
xsenemy <160485189+xsenemy@users.noreply.github.com>
2024-03-08 17:16:50 +0100

    Add files via upload

---
+++ day16.py
@@ -0,0 +1,9 @@
+#44 Programming questions Code
+#Very easy questions:
+#16) You have two arrays: A has 5 elements and B has 10 elements. Merge them into a single array.
+
+A = [1, 2, 3, 4, 5]
+B = [6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+C = A + B
+
+print(C)
