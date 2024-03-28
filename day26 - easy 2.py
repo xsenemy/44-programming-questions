cf04470cb10d32b6bf4467037def5f6ed344f2a9
xsenemy <160485189+xsenemy@users.noreply.github.com>
2024-03-28 18:19:53 +0100

    Update day26 - easy 2.py

--- day26 - easy 2.py
+++ day26 - easy 2.py
@@ -6,10 +6,22 @@ print("44 Programming questions Code - Easy 2\nhttps://github.com/xsenemy/44-pro
 
 
 def prime_factorization(n):
-    for i in range(1, n+1):
-        if n % i == 0:
-            print(i)
 
+    for i in range(2, n+1):
+        if n % i == 0:
+            prime = True
+            for j in range(2, int((i**0.5)+1)):
+                if i % j == 0:
+                    prime = False
+                    break
+            if prime:
+                print(f"{n} / {i}")
+                prime_factors.append(i)
+                prime_factorization(n=int(n / i))
+                break
 
 n = int(input("Int: "))
+prime_factors = []
 prime_factorization(n)
+print("Prime factors: ")
+print(*prime_factors, sep=" x ")
