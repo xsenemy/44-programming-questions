73d6b4d227d614e52464caea2ab5e5bbdc407586
xsenemy <160485189+xsenemy@users.noreply.github.com>
2024-04-01 22:05:09 +0200

    Add files via upload

---
+++ day28 - easy 4.py
@@ -0,0 +1,8 @@
+#44 Programming questions Code
+#Easy questions:
+#4) Generate a very, very large random number. Check how many digits it has.
+
+print("44 Programming questions Code - Easy 4\nhttps://github.com/xsenemy/44-programming-questions\n")
+
+n = 10**3000
+print(len(str(n)))
