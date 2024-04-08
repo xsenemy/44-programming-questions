454c410fc47e08214b2d47b67498a0d518ee7794
xsenemy <160485189+xsenemy@users.noreply.github.com>
2024-04-08 17:46:17 +0200

    Update day31 - easy 7.py

--- day31 - easy 7.py
+++ day31 - easy 7.py
@@ -10,17 +10,13 @@ nums = []
 ordered_nums = []
 for i in range(100):
     nums.append(random.randint(0, 100))
-int_x = 50
-if int_x not in nums:
-    print(nums)
-    print(f"{int_x} not in list")
-else:
-    int_x_index = nums.index(int_x)
-    for i in range(0, int_x_index + 1):
-        ordered_nums.append(nums[i])
-    n = 1
-    for i in range(int_x_index + 1, len(nums)):
-        ordered_nums.append(nums[-n])
-        n += 1
-    print(nums)
-    print(ordered_nums)
+int_x = 90
+int_x_index = nums.index(int_x)
+for i in range(0, int_x + 1):
+    ordered_nums.append(nums[i])
+n = 1
+for i in range(int_x + 1, len(nums)):
+    ordered_nums.append(nums[-n])
+    n += 1
+print(nums)
+print(ordered_nums)
